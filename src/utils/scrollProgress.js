ef9544d30e404d093265daec03550dd5df910489
Nazım <nazimardaarbac7@gmail.com>
2026-08-08 16:19:37 +0300

    Delay final burger hand pickup

--- src/utils/scrollProgress.js
+++ src/utils/scrollProgress.js
@@ -2,7 +2,7 @@ export const TRAY_SETTLE_END = 0.17
 export const MOBILE_TRAY_SETTLE_END = 0.075
 export const TRAY_ROTATION_START = 0.31
 export const TRAY_ROTATION_END = 0.94
-export const HAND_PICKUP_START = 0.92
+export const HAND_PICKUP_START = 0.936
 export const HAND_PICKUP_END = 1
 
 export function getTrayRotationProgress(progress) {
